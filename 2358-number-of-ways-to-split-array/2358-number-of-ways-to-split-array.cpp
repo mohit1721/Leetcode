11ccbb96c2cc70be7730c8e2fb5d2ef7fb726299
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-01-03 21:13:05 +0530

    Time: 0 ms (100%), Space: 89.2 MB (79.17%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.cpp
@@ -4,35 +4,28 @@ public:
         
 // 10 14 6 13 suffix. at i
 // 13 3 -1 7  prefix. at  n-i-1
-
+//opti.
 int n = nums.size();
-vector<long long int>prefixSum(n);
-vector<long long int>suffixSum(n);
-
- suffixSum[0] = nums[0];
-for(int i=1;i<n;i++)
+long long  totalSum =0;
+for(int num : nums)
 {
-   
-    suffixSum[i]= suffixSum[i-1] + nums[i];
-}
-prefixSum[n-1] = nums[n-1];
-for(int i=n-2;i>=0;i--)
-{
-    prefixSum[i] = prefixSum[i+1]+nums[i];
+    totalSum+=num;
 }
 
-int cnt=0;
-// 10 14 6 13 suffix. at i
-// 13 3 -1 7  prefix. at  n-i-1
 
-    for(int i=0;i<n-1;i++)
-    {
-        if(suffixSum[i]>= prefixSum[i+1] )
-        {
-            cnt++;
-        }
-    }
+long long  prefixSum=0;
+int ways =0;
+for(int i=0;i<n-1;i++)
+{
+    prefixSum+=nums[i];
+   long long  rightSum = totalSum- prefixSum;
+    if(prefixSum >= rightSum)
+        ways++;
 
-return cnt;
+} 
+
+return ways;
+
+
     }
 };
