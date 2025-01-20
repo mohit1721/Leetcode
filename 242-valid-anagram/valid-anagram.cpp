0993b83182d63adafd84e769ad8181ad3ceb8ed0
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-01-20 18:07:41 +0530

    Time: 4 ms (44.18%) | Memory: 9.7 MB (71.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-//my approach
-        vector<int> freq(26, 0);
+    bool isAnagram(string &s, string &t) {
+//my approach-->frq map lo..ek string k liye ++ , dusre k liye -- , baki re iterate..& check agar -ve/>=1 freq mile to false ...otherwise its true 
+ vector<int>freq(26,0);
+for(int i=0;i<s.length();i++)
+{
+    freq[s[i] - 'a']++;
+}
+for(int j=0;j<t.length();j++)
+{
+    freq[t[j] - 'a']--;
+}
+for(int i=0;i<26;i++)
+{
+    if(freq[i] <0 || freq[i] >=1)
+        return false;
+}
+return true;
 
-        for (int i = 0; i < s.length(); i++) {
-            freq[s[i] - 'a']++;
-        }
-        for (int i = 0; i < t.length(); i++) {
-            freq[t[i] - 'a']--;
-        }
-
-        for (int i = 0; i < freq.size(); i++) {
-            // if(s.length() > t.length())
-        //    {
-             if (freq[i] >= 1)
-                return  false;
-            // }
-            // else{
-                    if(freq[i] < 0) 
-                    return false;
-            }
-        // }
-        return true; //
     }
 
 }
