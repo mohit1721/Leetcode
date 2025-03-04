d86e26fc93b421f250f99a62e2c552022f545282
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-03-04 19:34:02 +0530

    Time: 6 ms (64.81%), Space: 127.8 MB (77.38%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        // better
-        // count less < pivots & equal to pivot... then set 3 pointers acc. to theri lengths
+        // 2 ptr
         int n =nums.size();
-        int less=0, equal =0 ;
-        for(int i=0;i<n ;i++){
-            if(nums[i] < pivot) less++;
-            else if(nums[i]==pivot) equal++;
+        
+        int less=0, greater=n-1;
+        int lessPtr=0, greaterPtr=n - 1;
+        vector<int>ans(n);
+        while(less <= n-1 && greater >= 0 ){
+            if(nums[less] < pivot){
+                    ans[lessPtr]= nums[less];
+                    lessPtr++;
+            }
+            if(nums[greater] > pivot){
+                ans[greaterPtr] = nums[greater];
+                greaterPtr--;
+            }
+             less++;
+             greater--;
         }
-    //   not set pointers acc. 
-    int lessPtr=0, equalPtr = less, greaterPtr = less + equal ;
-    vector<int>ans(n);
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i] < pivot && lessPtr <= less){
-                ans[lessPtr]=nums[i];
-                lessPtr++;
-            }
-            else if(nums[i] == pivot && equalPtr <= equal+less ){
-                ans[equalPtr]=nums[i];
-                equalPtr++;
-            }
-            else{
-                ans[greaterPtr] = nums[i];
-                greaterPtr++;
-            }
-        }
+// ab jo bachega, wo only == pivote honge..fill it
+while(lessPtr <= greaterPtr){
+    ans[lessPtr]=pivot;
+    lessPtr++;
+}
+return ans;
 
-        return ans;
     }
 };
