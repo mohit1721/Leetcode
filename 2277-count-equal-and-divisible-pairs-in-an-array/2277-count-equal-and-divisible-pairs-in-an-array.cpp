3390495faff9450c29535f635da83d0dd4a31429
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-04-17 10:32:33 +0530

    Time: 1 ms (42.39%), Space: 16.2 MB (7.3%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -3,14 +3,35 @@ public:
     int countPairs(vector<int>& nums, int k) {
 
 int n=nums.size();
-int count=0;
-for(int i=0;i<n;i++){
-    for(int j=i+1;j<n;j++){
-        if(nums[i]==nums[j] && (i*j)% k ==0) count++;
+// int count=0;
+// for(int i=0;i<n;i++){
+//     for(int j=i+1;j<n;j++){
+//         if(nums[i]==nums[j] && (i*j)% k ==0) count++;
+//     }
+// }
+// return count;
+  map<int, vector<int>> mp;
+    
+    // Step 1: Store all indices where each value occurs
+    for (int i = 0; i < nums.size(); i++) {
+        mp[nums[i]].push_back(i);
     }
-}
-return count;
 
+    int count = 0;
+
+    // Step 2: For each group of same elements, check index pairs
+    for (auto& [val, indices] : mp) {
+        int sz = indices.size();
+        for (int i = 0; i < sz; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                if ((indices[i] * indices[j]) % k == 0) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
 
 
 
