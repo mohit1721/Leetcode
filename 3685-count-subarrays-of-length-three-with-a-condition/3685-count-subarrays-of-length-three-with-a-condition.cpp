a360214af64f04f6cfc178108f21e3b444dc679a
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-04-27 08:57:39 +0530

    Time: 3 ms (18.47%), Space: 48.5 MB (11.51%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -2,12 +2,23 @@ class Solution {
 public:
     int countSubarrays(vector<int>& nums) {
     int n = nums.size();
- 
+    int i = 0, j = 0;
     int count = 0;
-    for(int i=0;i<n-2;i++){
-        if((nums[i]+ nums[i+2] ) *2 == nums[i+1]) count++;
+    
+    while (j < n) {
+        // Once we have a window of size 3
+        if (j - i + 1 == 3) {
+            // Check the condition for the triplet nums[i], nums[i+1], nums[i+2]
+            if ((nums[i] + nums[j] )*2== nums[i+1] ) {//---JAHIL ->
+                count++;  // If the condition is met, increase the count
+            }
+            // Shift the window by moving 'i' forward
+            i++;
+        }
+        // Always move 'j' forward to expand the window
+        j++;
     }
-     return count;
+    return count;
 }
 
 };
