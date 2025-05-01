69a15ca640fa9427d15a412670c7ca8c2e4bf490
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-01 15:15:40 +0530

    Time: 0 ms (100%), Space: 93.8 MB (68.72%) - LeetHub

--- 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-       long long result=0;
- 
-       int minPos=-1;
-       int maxPos=-1;
-       int cultPos=-1;//indexes
-       int n=nums.size();
-      
-    for(int i=0;i<n;i++)
-    {
-        // out of range-->culprit
-        if(nums[i]<minK || nums[i]>maxK)
-            cultPos=i;
-        
-        if(nums[i]==minK)
-            minPos=i;
-        if(nums[i]==maxK)
-            maxPos=i; 
+        long long result = 0;
 
-            //  and ,find result on each iteration
-            int temp=min(minPos,maxPos)-cultPos;
-            if(temp<0) temp=0;
-            result+=temp;
-    }
+        int minKPos = -1;
+        int maxKPos = -1;
+        int problematicPos = -1; // indexes
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            // CHECK OUT OF RANGE H to -> problematic idx
+            if (nums[i] < minK || nums[i] > maxK) {
+
+                problematicPos = i;
+            }
+            // else check of other two positions
+            if (nums[i] == minK)
+                minKPos = i;
+            if (nums[i] == maxK) {
+                maxKPos = i;
+            }
+            // calculate result on each iteration
+            int smallerOfMin_MaxKPos = min(minKPos, maxKPos);
+            // number of subarray will
+            int numOfSubarrays = smallerOfMin_MaxKPos - problematicPos;
+
+            // if its negative, means no favourable subarray[0] ->
+            // culprit/problematicPos subarray k bich m hoga
+            result += (numOfSubarrays < 0) ? 0 : numOfSubarrays;
+        }
 
         return result;
-
     }
 };
