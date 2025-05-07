9c464b020bbc4ddd5b5dcc8fe3bfc327d2794c35
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-07 20:05:20 +0530

    Time: 0 ms (100%), Space: 13.2 MB (76.53%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    bool binarySearch(vector<int>& nums, int& target)
-    {
-        int s = 0, e= nums.size();
-        while(s<=e)
-        {
-            int mid = s+ ( e - s) /2;
-            if(target == nums[mid])
-            {
+    bool binarySearch(vector<int>& nums, int& target) {
+        int s = 0, e = nums.size();
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (target == nums[mid]) {
                 return true;
-            }
-            else if (nums[mid] < target)
-            {
-                s= mid+1;
-            }
-            else{
+            } else if (nums[mid] < target) {
+                s = mid + 1;
+            } else {
                 e = mid - 1;
             }
         }
@@ -22,20 +16,28 @@ public:
     }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
-        int n=matrix.size();
-        int m=matrix[0].size();
 
-        for(int i=0;i<n;i++)
-        {
-            if(target >= matrix[i][0] && target <= matrix[i][m-1])  //*
-            {
-                return binarySearch(matrix[i] , target);
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int l=0, h = m*n - 1;
+        while(l<=h){
+            int mid = l + (h-l) /2;
+            int row = mid/m;
+            int col = mid% m;
+
+            if(matrix[row][col]==target){
+                return true;
             }
-            // else
-
+            else if(matrix[row][col]  < target ){
+                l = mid  + 1;
+            }
+            else{
+                h = mid - 1;
+            }
         }
-return false;
-        
+
+      
+        return false;
     }
 };
