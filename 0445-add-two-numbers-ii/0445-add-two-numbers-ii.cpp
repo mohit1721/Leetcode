cd195bfced5332c380bd84a78696df9ae6057f85
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-09 17:01:37 +0530

    Time: 4 ms (35.03%), Space: 79.5 MB (7.52%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -38,18 +38,22 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     ListNode* ansTail = NULL;
     int carry = 0;
 
-    // Case a: Both lists have the same length
-    while (!s1.empty() || !s2.empty()) {
+     while (!s1.empty() || !s2.empty()) {
+
         int l1val = !s1.empty() ? s1.top() : 0;
         if(!s1.empty())
             s1.pop();
         int l2val = !s2.empty() ? s2.top() : 0;
         if(!s2.empty())
             s2.pop();
+
         int sum = carry + l1val + l2val;
+
         int digit = sum % 10;
         carry = sum / 10;
+
         ListNode* newNode = new ListNode(digit);
+        //1st node
         if (!ansHead) {
             ansHead = newNode;
             ansTail = newNode;
@@ -57,49 +61,18 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
             ansTail->next = newNode;
             ansTail = newNode;
         }
-        // l1 = l1->next;
-        // l2 = l2->next;
     }
-
-    // // Case b: Remaining nodes in l1
-    // while (!s1.empty()) {
-    //     int l1val = s1.top();
-    //     s1.pop();
-    //     int sum = carry + l1val;
-    //     int digit = sum % 10;
-    //     carry = sum / 10;
-    //     ListNode* newNode = new ListNode(digit);
-    //     ansTail->next = newNode;
-    //     ansTail = newNode;
-    //     // l1 = l1->next;
-    // }
-
-    // // Case c: Remaining nodes in l2
-    // while (!s2.empty()) {
-    //     int l2val = s2.top();
-    //     s2.pop();
-    //     int sum = carry + l2val;
-    //     int digit = sum % 10;
-    //     carry = sum / 10;
-    //     ListNode* newNode = new ListNode(digit);
-    //     ansTail->next = newNode;
-    //     ansTail = newNode;
-    //     // l2 = l2->next;
-    // }
-
     // Case d: Carry remaining
     if (carry != 0) {
-        int sum = carry;
-        int digit = sum % 10;
-        carry = sum / 10;
-        ListNode* newNode = new ListNode(digit);
+        // int sum = carry;
+        // int digit = sum % 10;
+        // carry = sum / 10;
+        ListNode* newNode = new ListNode(carry);
         ansTail->next = newNode;
         ansTail = newNode;
     }
-
     // ek baar reverse
     ansHead = reverse(ansHead);
     return ansHead;
 }
-}
-;
+};
