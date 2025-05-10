1449cbad4b2bd349f7c3fbba6d5340cc6b29a590
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-10 23:04:45 +0530

    Time: 0 ms (100%), Space: 14 MB (87.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,70 +1,86 @@
 class Solution {
 public:
-    int solveMem(vector<int>&nums, int n , int curr , int prev,  vector<vector<int>>&dp){
-        if(curr>=n) return 0;
-        if(dp[curr][prev+1] != -1) return dp[curr][prev+1];
+    int solveMem(vector<int>& nums, int n, int curr, int prev,
+                 vector<vector<int>>& dp) {
+        if (curr >= n)
+            return 0;
+        if (dp[curr][prev + 1] != -1)
+            return dp[curr][prev + 1];
         int include = 0;
 
-        if(prev == -1 || nums[curr] > nums[prev]){
-            include = 1  + solveMem(nums, n, curr+1 , curr , dp);
+        if (prev == -1 || nums[curr] > nums[prev]) {
+            include = 1 + solveMem(nums, n, curr + 1, curr, dp);
         }
-        int exclude = 0 + solveMem(nums, n, curr+1 , prev, dp);
+        int exclude = 0 + solveMem(nums, n, curr + 1, prev, dp);
 
-        return dp[curr][prev+1] =  max(include, exclude);
+        return dp[curr][prev + 1] = max(include, exclude);
     }
-    int lisTab(vector<int>& nums){
-              int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+    int lisTab(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-    for(int curr=n-1;curr>=0;curr--){
-        for(int prev= curr-1;prev>=-1;prev--){//optimize-> prev = n-1 -> curr - ;
-    int include = 0;
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1;
+                 prev--) { // optimize-> prev = n-1 -> curr - ;
+                int include = 0;
 
-        if(prev == -1 || nums[curr] > nums[prev]){
-            include = 1  + dp[curr+1][curr+1]; //***change2
-        }
-        int exclude = 0 + dp[curr+1][prev+1]; //***change1
+                if (prev == -1 || nums[curr] > nums[prev]) {
+                    include = 1 + dp[curr + 1][curr + 1]; //***change2
+                }
+                int exclude = 0 + dp[curr + 1][prev + 1]; //***change1
 
-        dp[curr][prev+1] =  max(include, exclude);
-
-        }
-    }
-
-return dp[0][0];
-    }
-int tabSO(vector<int>&nums){
-    int n=nums.size();
-    vector<int>current(n+1,0); //dp[curr]
-    vector<int>next(n+1,0); //dp[curr+1]
-
-    for(int curr=n-1;curr>=0;curr--){
-        for(int prev=curr-1;prev>=-1;prev--){
-            
-            int include = 0;
-            if(prev==-1 || nums[curr]>nums[prev]){
-                include = 1 + next[curr + 1];
+                dp[curr][prev + 1] = max(include, exclude);
             }
-            int exclude = 0 + next[prev + 1];
-
-            current[prev+1] = max(include, exclude);
         }
-        //shift***
-        next = current;
 
+        return dp[0][0];
     }
+    int tabSO(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> current(n + 1, 0); // dp[curr]
+        vector<int> next(n + 1, 0);    // dp[curr+1]
 
+        for (int curr = n - 1; curr >= 0; curr--) {
+            for (int prev = curr - 1; prev >= -1; prev--) {
 
-return current[0];
-}
+                int include = 0;
+                if (prev == -1 || nums[curr] > nums[prev]) {
+                    include = 1 + next[curr + 1];
+                }
+                int exclude = 0 + next[prev + 1];
+
+                current[prev + 1] = max(include, exclude);
+            }
+            // shift***
+            next = current; // shifting back**
+        }
+
+        return current[0];
+    }
 
     int lengthOfLIS(vector<int>& nums) {
         // int n = nums.size();
         // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
 
         // return solveMem(nums, n , 0, -1, dp);
-        return tabSO(nums);
+        // return tabSO(nums);
 
+        //  #MOST OPTIMAL USING BINARY SEARCH
+        vector<int> temp;
+        for (int currEle : nums) {
+            // Find index of the first element >= num
+            int indexFirstEle =
+                lower_bound(temp.begin(), temp.end(), currEle) - temp.begin();
 
+            if (indexFirstEle == temp.size()) {
+                // num is greater than all elements in temp, safe to append
+                temp.push_back(currEle);
+            } else {
+                // Replace the first element >= num
+                temp[indexFirstEle] = currEle;
+            }
+        }
 
+        return temp.size();
     }
 };
