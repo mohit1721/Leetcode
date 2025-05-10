85bf756140b392ddc2bc6b9edab8178900732a9d
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-10 21:58:05 +0530

    Time: 515 ms (21.25%), Space: 296.4 MB (19.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -12,11 +12,34 @@ public:
 
         return dp[curr][prev+1] =  max(include, exclude);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+    int lisTab(vector<int>& nums){
+              int n = nums.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
 
-        return solveMem(nums, n , 0, -1, dp);
+    for(int curr=n-1;curr>=0;curr--){
+        for(int prev= n-1;prev>=-1;prev--){
+    int include = 0;
+
+        if(prev == -1 || nums[curr] > nums[prev]){
+            include = 1  + dp[curr+1][curr+1]; //***change
+        }
+        int exclude = 0 + dp[curr+1][prev+1]; //***change
+
+        dp[curr][prev+1] =  max(include, exclude);
+
+        }
+    }
+
+return dp[0][0];
+    }
+
+
+    int lengthOfLIS(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+
+        // return solveMem(nums, n , 0, -1, dp);
+        return lisTab(nums);
 
 
 
