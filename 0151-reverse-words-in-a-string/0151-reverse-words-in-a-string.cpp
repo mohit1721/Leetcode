71ee45d979ff8f3e6ed3e7e5f828f0b6468eb175
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-11 11:17:01 +0530

    Time: 3 ms (41.88%), Space: 10.1 MB (76.88%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,47 +1,35 @@
 class Solution {
 public:
+    // Accolite
     string reverseWords(string& s) {
-        //
- // A. Reverse whole String s
-// jb tk character milta h assign to r
- // else,agar space milta h to,reverse the word b.w l & r
-//  r ko v to aage badhao** gltiii
-// l ko v to ab aage badhana h ..jo ki ab r k paas ayega
- // return only r-1
+        // A. Reverse whole String s
+        // jb tk character milta h assign to r
+        // else,agar space milta h to,reverse the word b.w l & r
+        //  r ko v to aage badhao** gltiii
+        // l ko v to ab aage badhana h ..jo ki ab r k paas ayega
+        // return only r-1
 
         int n = s.length();
-        int i = 0;
-        // A. Reverse
+        int i=0;
+        int l=0,r=0;
         reverse(s.begin(), s.end());
-        // 
-        int l=0, r=0;
-        while(i<n)
-        {
-            while(i<n && s[i] != ' ')//jbtk space n mile
-            {
-                s[r++]= s[i++];
-            }
-         //else,agar space milta h to,reverse the word b.w l & r
 
-            if(l<r)
-            {
-        // reverse this word now
-            reverse(s.begin()+l, s.begin()+r); //l to r
-            // last m ek space add
-            s[r]=' ';
-            r++;
-            l=r; //l ko aage badha do
-            }
-    i++;
+        while(i<n){
+                while(i< n && s[i]!= ' '){
+                    s[r++]=s[i++];
+                }
+                //space mila
+                if(l<r){
+                    reverse(s.begin()+l, s.begin()+r);
+                    
+                    s[r]=' ';
+                    r++;
+                    l=r;
 
-
-
+                }
+                i++;
         }
 
-return s.substr(0, r-1);//last space exclue;
-
-
-
-
+        return s.substr(0, r - 1);
     }
 };
