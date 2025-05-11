38a89ea263b26753e0b4212e9dd0f1e785c230b4
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-11 11:36:53 +0530

    Time: 3 ms (41.88%), Space: 11.3 MB (30.48%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     // Accolite
     string reverseWords(string& s) {
-        // A. Reverse whole String s
-        // jb tk character milta h assign to r
-        // else,agar space milta h to,reverse the word b.w l & r
-        //  r ko v to aage badhao** gltiii
-        // l ko v to ab aage badhana h ..jo ki ab r k paas ayega
-        // return only r-1
-
-        int n = s.length();
-        int i=0;
-        int l=0,r=0;
-        reverse(s.begin(), s.end());
-
-        while(i<n){
-                while(i< n && s[i]!= ' '){
-                    s[r++]=s[i++];
-                }
-                //space mila
-                if(l<r){
-                    reverse(s.begin()+l, s.begin()+r);
-                    
-                    s[r]=' ';
-                    r++;
-                    l=r;
-
-                }
-                i++;
+       vector<string> ans;
+        stringstream ss(s);
+        string word;
+        
+        while (ss >> word) {
+            ans.push_back(word);
         }
 
-        return s.substr(0, r - 1);
+        reverse(ans.begin(), ans.end());
+
+        string res = "";
+        for (int i = 0; i < ans.size(); i++) {
+            res += ans[i];
+            if (i != ans.size() - 1)
+                res += ' ';
+        }
+
+        return res;
+
+
     }
 };
