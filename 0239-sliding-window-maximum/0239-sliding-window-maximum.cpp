6c7305e951f8cbf24f6caca8b55c5b58441154e9
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-05-31 18:45:38 +0530

    Time: 170 ms (13.24%), Space: 198.5 MB (7.34%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n=nums.size();
-        list<int>maxElements;
-        vector<int>ans;
-        int l=0,r=0;
-        while(r<n)
-        {
-            // do calc. but before check for right position to insert [as I need to maintain the maxElements in list]
-            while(!maxElements.empty() && maxElements.back() < nums[r] )
-                maxElements.pop_back();//
-            
-            // then push the current ele
+        int n = nums.size();
+        list<int> maxElements;
+        vector<int> ans;
+        int l = 0, r = 0;
+        while (r < n) {
+            while (!maxElements.empty() &&
+                   maxElements.back() <
+                       nums[r]) { // maxStore kr rhe , to max hi rkho, chhote ko
+                                  // q rkhna**
+                maxElements.pop_back(); //
+            }
+
             maxElements.push_back(nums[r]);
 
-            // if not in Wsize
-            if(r-l+1 < k)
-            {
+            if (r - l + 1 < k) {
                 r++;
-            }
-            else {
-                // >=
+            } else { // got window
                 int maxElementInCurrentWindow = maxElements.front();
                 ans.push_back(maxElementInCurrentWindow);
-                // also , before sliding, check the left part of window yaani nums[s], that kya jo abhi ka maxElementInCurrentWindow hi to nhi h jisko hatana h
-                if(maxElementInCurrentWindow == nums[l])
-                {
-                    maxElements.pop_front();//age m rhega obv. [maxElement h to].qki algorith hi waisa  ki maxElement front m hi hpga.
+                // slide..
+                if (maxElementInCurrentWindow == nums[l]) {
+                    maxElements.pop_front();
                 }
-                // now u can slide
                 l++;
                 r++;
-
             }
-
         }
         return ans;
     }
